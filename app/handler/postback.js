dd8bdaacf40206b2b1adcd2dc572efad947bfa72
fsexchangerrates <fs.suppermaster@gmail.com>
2020-05-09 14:38:49 +0700

    update

--- app/handler/postback.js
+++ app/handler/postback.js
@@ -2,10 +2,11 @@ const currency = require("./../messages/currency.json")
 const paypal = require('./../messages/paypal.json')
 const webmoney = require('./../messages/webmoney.json')
 const greeting = require('./../messages/greeting.json')
+const richMenuId_1 = 'xsdfffffffff'
+const richMenuId_2 = 'vvvvvvvvvv'
 
 function handle(client, event) {
-    var richMenuId_1 = 'xsdfffffffff'
-    var richMenuId_2 = 'vvvvvvvvvv'
+    var userId = event.source.userId
     var data = event.postback.data
     switch (data) {
         case 'NextMenu':
@@ -19,7 +20,8 @@ function handle(client, event) {
         case 'webmoney':
             return client.replyMessage(event.replyToken, webmoney);
         default:
-            console.log(client.linkRichMenuToUser, client.replyMessage)
+            console.log(client, userId, event.replyToken, `Got postback data: ${data}`);
+            return client.replyMessage(event.replyToken, greeting), client.linkRichMenuToUser(event.source.userId, richMenuId_1);
     }
 }
 
